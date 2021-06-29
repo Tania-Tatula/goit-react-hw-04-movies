ec040c31eade0f01ccb01e6fa0d44860cca06b9a
Tania Tatula <tgrogol@gmail.com>
2021-06-29 19:20:48 +0300

    Updates

--- static/js/7.51c47644.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[7],{71:function(t,e,i){"use strict";i.r(e);var n=i(1);e.default=function(){return Object(n.jsx)("h1",{children:"404 not found"})}}}]);
-//# sourceMappingURL=7.51c47644.chunk.js.map
